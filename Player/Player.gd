008937ba13e97b320329ad632848cefff55dbf13
dubena <andrej.duben@accenture.com>
2022-03-13 11:40:02 +0100

    Added player movement + collision shape

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,14 +1,23 @@
 extends KinematicBody2D
 
+const ACCELERATION = 10.0
+const MAX_SPEED = 100.0
+const FRICTION = 10.0
+
 var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
-var speed = 100.0
 
 func _physics_process(delta):
-	move_and_slide(_get_velocity())	
+	velocity = move_and_slide(_get_velocity())	
 
 func _get_velocity():
-	return _get_direction() * speed
+	direction = _get_direction()
+	if direction != Vector2.ZERO:
+		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+		
+	return velocity
 	
 func _get_direction():
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
