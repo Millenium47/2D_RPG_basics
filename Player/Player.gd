b40ec08e2001afd2d8cfc185c9a94ea996958936
dubena <andrej.duben@accenture.com>
2022-03-14 15:15:44 +0100

    Added animation tree for player

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,9 @@ const FRICTION = 10.0
 var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
 
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 func _physics_process(delta):
 	velocity = move_and_slide(_get_velocity())	
 
@@ -14,7 +17,11 @@ func _get_velocity():
 	direction = _get_direction()
 	if direction != Vector2.ZERO:
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
+		animationTree.set("parameters/Idle/blend_position", velocity)
+		animationTree.set("parameters/Run/blend_position", velocity)
+		animationState.travel("Run")
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 		
 	return velocity
