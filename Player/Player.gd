95e6cdfac13809062077a8f722827dc99b44e8a2
dubena <andrej.duben@accenture.com>
2022-03-16 14:07:35 +0100

    Added roll

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 const ACCELERATION = 10.0
 const MAX_SPEED = 100.0
+const MAX_ROLL_SPEED = 120.0
 const FRICTION = 10.0
 
 enum {
@@ -13,6 +14,7 @@ enum {
 var state = MOVE
 var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
+var roll_direction = Vector2.LEFT
 
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
@@ -25,17 +27,25 @@ func _physics_process(delta):
 		MOVE:
 			move_state()
 		ROLL:
-			pass
+			roll_state()
 		ATTACK:
 			attack_state()
 		
 func attack_state():
 	velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
-	velocity = move_and_slide(velocity)
 	animationState.travel("Attack")
+	_move()
+	
+func roll_state():
+	velocity = roll_direction * MAX_ROLL_SPEED
+	animationState.travel("Roll")
+	_move()
 	
 func move_state():
 	velocity = move_and_slide(_get_velocity())	
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
@@ -43,10 +53,12 @@ func move_state():
 func _get_velocity():
 	direction = _get_direction()
 	if direction != Vector2.ZERO:
+		roll_direction = direction
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
 		animationTree.set("parameters/Idle/blend_position", velocity)
 		animationTree.set("parameters/Run/blend_position", velocity)
 		animationTree.set("parameters/Attack/blend_position", velocity)
+		animationTree.set("parameters/Roll/blend_position", velocity)
 		animationState.travel("Run")
 	else:
 		animationState.travel("Idle")
@@ -63,5 +75,12 @@ func _get_direction():
 	
 	return direction
 
+func _move():
+	velocity = move_and_slide(velocity)
+
 func _attack_animation_finished():
+	state = MOVE
+
+func _roll_animation_finished():
+	velocity = velocity / 2
 	state = MOVE
